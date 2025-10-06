a30a70856531b40e14986c7ca60497e38c6c4708
guifernandes0525 <guifernandes0525@gmail.com>
2025-10-06 11:50:31 +0200

    merged modifications of server with udp and robust string input response

--- src/server.c
+++ src/server.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <netinet/udp.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
 #include <errno.h>
@@ -15,6 +16,8 @@
 #define BUFF_LEN 1024
 #define MAX_CLIENT 10
 #define MAX_PORT_LEN 32
+#define TCP 1
+#define UDP 0
 
 int main(int argc, char *argv[]) {
 
@@ -23,6 +26,7 @@ int main(int argc, char *argv[]) {
     timeout.tv_usec = 0;
 
     int serv_port = -1;
+    int protocol = 0;
 
     if (argc < 3 || atoi(argv[1]) < 22000 || atoi(argv[2]) < 0){
         do {
@@ -32,14 +36,24 @@ int main(int argc, char *argv[]) {
     }
     else serv_port = atoi(argv[1]);
 
-    int server_fd;
+    if (argc == 3){
+        if (strcmp(argv[2], "tcp") == 0 || strcmp(argv[2], "TCP") == 0)
+            protocol = TCP;
+        else if (strcmp(argv[2], "udp") == 0 || strcmp(argv[2], "UDP") == 0)
+            protocol = UDP;
+    }
+    else protocol = UDP;
 
-    // SOCK_STREAM is TCP
-    server_fd = socket(AF_INET, SOCK_STREAM, 0); 
+    int server_fd;
+    // SOCK_STREAM is TCP == 0. SOCK_DGRAM is UDP == 1;
+    if (protocol) server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    else server_fd = socket(AF_INET, SOCK_DGRAM, 0);     
     
     if (server_fd == -1) return -1;
 
-    struct sockaddr_in serv_address;
+    
+    struct sockaddr_in serv_address, client_address;
+    
     // ipv4
     serv_address.sin_family = AF_INET; 
     serv_address.sin_port = htons(serv_port);
@@ -60,22 +74,21 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    listen(server_fd, MAX_CLIENT);
+    if (protocol)
+        listen(server_fd, MAX_CLIENT);
     
-    struct sockaddr_in client_address;
-
     while (true)
     {
-        int calc_socket;
+        int calc_socket = 0;
         socklen_t client_addr_size = sizeof(client_address);
-        calc_socket = accept(server_fd, (struct sockaddr *) &client_address, &client_addr_size);
-
-        printf("[+] %s\n", inet_ntoa(client_address.sin_addr));
-
-        if (setsockopt(calc_socket, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0) {
-            printf("Problem setting socket option");
-        }
         
+        if (protocol) {
+            calc_socket = accept(server_fd, (struct sockaddr *) &client_address, &client_addr_size); 
+            if (setsockopt(calc_socket, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0) {
+                printf("Problem setting socket option");
+            }
+        }
+
         if (calc_socket == -1) {
             printf("Problem accepting connection");
         }
@@ -84,7 +97,8 @@ int main(int argc, char *argv[]) {
 
         if (pid == 0) { 
             
-            close(server_fd); 
+            if (protocol)
+                close(server_fd); 
 
             int Inactivity = 0;
 
@@ -92,9 +106,12 @@ int main(int argc, char *argv[]) {
             char recv_buffer[BUFF_LEN];
 
             EXPRESSION expression;
-
-            while (1) {
-                ssize_t n = recv(calc_socket, recv_buffer, BUFF_LEN, 0);
+            do { 
+                ssize_t n;
+                if (protocol)
+                    n = recv(calc_socket, recv_buffer, BUFF_LEN, 0);
+                else 
+                    n = recvfrom(calc_socket, recv_buffer, BUFF_LEN, 0, (struct sockaddr*)&client_address, &client_addr_size);
 
                 if (n == 0) break;
 
@@ -116,7 +133,6 @@ int main(int argc, char *argv[]) {
                 
                 int m;
                 if (format_input(recv_buffer, &expression, inet_ntoa(client_address.sin_addr))) {
-                    
                     m = snprintf(send_buffer, BUFF_LEN, "Invalid input format.\n");
                 }
                 else if (invalid_range(expression)) {
@@ -126,8 +142,13 @@ int main(int argc, char *argv[]) {
                     calculate(&expression);
                     m = snprintf(send_buffer, BUFF_LEN, "%s%.2f\n", expression.message, expression.result);
                 }
-                send(calc_socket, send_buffer, (size_t) m, 0);
-            }
+                
+                if (protocol)
+                    send(calc_socket, send_buffer, (size_t) m, 0);
+                else
+                    sendto(calc_socket, send_buffer, (size_t) m, 0, (struct sockaddr*)&client_address, client_addr_size);
+
+            } while (protocol);
 
             close(calc_socket);
 
