83c8e2f02b45694dce47a45ec482c959386718ac
guifernandes0525 <guifernandes0525@gmail.com>
2025-10-06 19:49:51 +0200

    working final versions of udp client and server

--- src/client_udp.c
+++ src/client_udp.c
@@ -6,8 +6,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#define TCP 0
-#define UDP 1
+#define BUFF_LEN 1024
 
 void remove_spaces(char* s) {
         char* d = s;
@@ -21,62 +20,57 @@ void remove_spaces(char* s) {
 
 int main(int argc, char* argv[]) {
 
+    if (argc != 3) {
+        printf("2 input is needed : ./clientCalc IPServer Port\n");
+        return -1;
+    }
+
     int client_socket = socket(AF_INET, SOCK_DGRAM, 0);
 
-        if (client_socket < 0) {
-            printf("Error during socket creating process\n");
-            return -1;
-        }
-
-        struct sockaddr_in serv_addr;
-
-        memset(&serv_addr, 0, sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(atoi(argv[2]));
-
-        if (inet_aton(argv[1], &serv_addr.sin_addr) == 0) {
-            printf("Erreur lecture IP\n");
-            return -1;
-        };
-
-        char* send_buffer = (char*) malloc(1024*sizeof(char));
-        char* recv_buffer = (char*) malloc(1024*sizeof(char));
-
-        while (1) {
-            printf("Enter the calcul : ");
-
-            if (fgets(send_buffer, 1024, stdin) == NULL) {
-                fprintf(stderr, "Error reading\n");
-                break;
-            }
-
-            send_buffer[strcspn(send_buffer, "\n")] = '\0';
-
-            remove_spaces(send_buffer);
-
-            if (strcmp(send_buffer, "quit") == 0) break;
-
-            size_t len = strlen(send_buffer);
-            if (sendto(client_socket, send_buffer, len, 0, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1) {
-                printf("Error during message sending\n");
-                break;
-            }
-
-            socklen_t addr_size = sizeof(addr_size);
-
-            ssize_t n = recvfrom(client_socket, recv_buffer, 1023, 0, (struct sockaddr*) &serv_addr, &addr_size);
-            if (n <= 0) {
-                printf("Error during message reception\n");
-                break;
-            }
-            recv_buffer[n] = '\0';
-
-            printf("%s\n", recv_buffer);
-        }
-
-        close(client_socket);
-        free(send_buffer);
-        free(recv_buffer);
-
-        return 0;
+    if (client_socket < 0) {
+        printf("Error during socket creating process\n");
+        return -1;
     }
+
+    struct sockaddr_in serv_addr;
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if (inet_aton(argv[1], &serv_addr.sin_addr) == 0) {
+        printf("IP read error \n");
+        return -1;
+    };
+
+    char send_buffer[BUFF_LEN];
+    char recv_buffer[BUFF_LEN];
+
+    printf("Enter the calcul : ");
+
+    if (fgets(send_buffer, 1024, stdin) == NULL) {
+        fprintf(stderr, "Error reading\n");
+    }
+
+    send_buffer[strcspn(send_buffer, "\n")] = '\0';
+
+    remove_spaces(send_buffer);
+
+    size_t len = strlen(send_buffer);
+    if (sendto(client_socket, send_buffer, len, 0, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1) {
+        printf("Error during message sending\n");
+    }
+
+    socklen_t addr_size = sizeof(addr_size);
+
+    ssize_t n = recvfrom(client_socket, recv_buffer, 1023, 0, (struct sockaddr*) &serv_addr, &addr_size);
+    if (n <= 0) {
+        printf("Error during message reception\n");
+    }
+    recv_buffer[n] = '\0';
+
+    printf("%s\n", recv_buffer);
+
+    close(client_socket);
+    return 0;
+}
--- src/server_udp.c
+++ src/server_udp.c
@@ -16,17 +16,10 @@
 #define BUFF_LEN 1024
 #define MAX_CLIENT 10
 #define MAX_PORT_LEN 32
-#define TCP 1
-#define UDP 0
 
 int main(int argc, char *argv[]) {
 
-    struct timeval timeout;
-    timeout.tv_sec = 300;
-    timeout.tv_usec = 0;
-
     int serv_port = -1;
-    int protocol = 0;
 
     if (argc < 3 || atoi(argv[1]) < 22000){
         do {
@@ -50,7 +43,6 @@ int main(int argc, char *argv[]) {
     serv_address.sin_addr.s_addr = INADDR_ANY;
 
     int opt = 1;
-
     if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         printf("Problem setting options for socket\n");        
         return -1;
@@ -65,33 +57,22 @@ int main(int argc, char *argv[]) {
     
     while (true)
     {
-        int calc_socket = 0;
         socklen_t client_addr_size = sizeof(client_address);
-
-        if (calc_socket == -1) {
-            printf("Problem accepting connection");
-        }
         
-        char send_buffer[BUFF_LEN];
-        char recv_buffer[BUFF_LEN];
+        char send_buffer_child[BUFF_LEN];
+        char recv_buffer_child[BUFF_LEN];
             
         ssize_t n;
-        n = recvfrom(calc_socket, recv_buffer, BUFF_LEN, 0, (struct sockaddr*)&client_address, &client_addr_size);
+        n = recvfrom(server_fd, recv_buffer_child, BUFF_LEN, 0, (struct sockaddr*)&client_address, &client_addr_size);
 
-        pid_t pid = fork();
+        pid_t pid; 
+        if (n > 0) {
+            pid = fork();
+        }
 
         if (pid == 0) {
-            
-            char send_buffer_child[BUFF_LEN];
-            char recv_buffer_child[BUFF_LEN];
-            
-            size_t n_child = n;
-            strcpy(send_buffer_child, send_buffer);
-            strcpy(recv_buffer_child, recv_buffer);
 
             EXPRESSION expression;        
-
-            if (n < 0) printf("Problem receiving info");
                 
             recv_buffer_child[n] = '\0';
 
@@ -107,18 +88,12 @@ int main(int argc, char *argv[]) {
                 m = snprintf(send_buffer_child, BUFF_LEN, "%s%.2f\n", expression.message, expression.result);
             }
                 
-            sendto(calc_socket, send_buffer_child, (size_t) m, 0, (struct sockaddr*)&client_address, client_addr_size);
+            sendto(server_fd, send_buffer_child, (size_t) m, 0, (struct sockaddr*)&client_address, client_addr_size);
 
-            close(calc_socket);
+            close(server_fd);
 
             exit(0);
 
-        }
-        else if (pid > 0) { // this is the father process
-            close(calc_socket);
-        }
-        else {
-            close(calc_socket);
         }
     }
 
